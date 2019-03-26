b02d6e342586c8dfaa1f895161adaca1b79f1b68
easypainttou <jiajiaaiai@163.com>
2019-03-26 12:59:57 +0800

    update projects/04

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@sum
+M=0
+@i
+M=1
+(LOOP)
+@i
+D=M
+@R0
+D=D-M
+@END
+D;JGT
+@R1
+D=M
+@sum
+M=M+D
+@i
+M=M+1
+@LOOP
+0;JMP
+(END)
+@sum
+D=M
+@R2
+M=D
